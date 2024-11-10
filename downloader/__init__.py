e43a3bbff863461665a8eb592c4d300bf7db50ae
Daniel Parks <dp+git@oxidized.org>
2024-11-09 19:21:48 -0800

    Generate Anki deck from downloaded videos.

--- downloader/__init__.py
+++ downloader/__init__.py
@@ -1,102 +1,46 @@
-from ffmpeg import FFmpeg
-import yt_dlp
-import json
+import genanki
+import hashlib
+import fileinput
+import html
 import os
-import urllib
+
+from video import Video
 
 CACHE = './cache'
-YT_DLP_OPTIONS = {
-  'quiet': True,
-  'skip_unavailable_fragments': False,
-}
-
 os.makedirs(CACHE, exist_ok=True)
 
-def yt_url_to_id(url):
-  url_info = urllib.parse.urlparse(url)
-  query = urllib.parse.parse_qs(url_info.query)
-  if len(query['v']) != 1:
-    raise ValueError(f"Expected exactly one v parameter in URL: {url}")
-  return query['v'][0]
+def name_to_id(name):
+  bytes = hashlib.sha256(name.encode('utf-8')).digest()
+  # Apparently deck ID is i64
+  return int.from_bytes(bytes[:8], byteorder='big', signed=True)
 
-class Logger:
-  def __init__(self):
-    self.level = 0
-
-  def info(self, message):
-    if self.level >= 0:
-      print(message)
-
-LOGGER = Logger()
-
-class Video:
-  def __init__(self, url):
-    self.url = url
-    self.id = yt_url_to_id(url)
-    self._info = None
-
-  def info_cache_path(self):
-    return os.path.join(CACHE, self.id + '_info.json')
-
-  def raw_video_cache_path(self, logger=LOGGER):
-    return os.path.join(CACHE, self.id + '_raw.' + self.info(logger=logger)['ext'])
-
-  def processed_video_cache_path(self):
-    return os.path.join(CACHE, self.id + '_processed.mp4')
-
-  def info(self, logger=LOGGER):
-    if self._info is None:
-      try:
-        with open(self.info_cache_path(), 'r', encoding="utf-8") as file:
-          self._info = json.load(file)
-      except FileNotFoundError:
-        with yt_dlp.YoutubeDL(YT_DLP_OPTIONS.copy()) as ydl:
-          logger.info(f"{self.id}: getting info")
-          self._info = ydl.extract_info(self.url, download=False)
-          with open(self.info_cache_path(), 'w', encoding="utf-8") as file:
-            file.write(json.dumps(ydl.sanitize_info(self._info)))
-    return self._info
-
-  def raw_video(self, logger=LOGGER):
-    path = self.raw_video_cache_path(logger=logger)
-    if os.path.exists(path) and os.stat(path).st_size > 0:
-      return path
-
-    logger.info(f"{self.id}: downloading raw video to {path}")
-    options = {
-      'outtmpl': {
-        'default': path,
-      },
-      **YT_DLP_OPTIONS,
-    }
-
-    with yt_dlp.YoutubeDL(options) as ydl:
-      # FIXME why not use the in-memory info?
-      error_code = ydl.download_with_info_file(self.info_cache_path())
-      if error_code:
-        # FIXME??!
-        raise RuntimeError(error_code)
-
-    return path
-
-  def processed_video(self, logger=LOGGER):
-    output_path = self.processed_video_cache_path()
-    if os.path.exists(output_path) and os.stat(output_path).st_size > 0:
-      return output_path
-
-    raw_path = self.raw_video(logger=logger)
-
-    logger.info(f"{self.id}: processing video")
-    FFmpeg().option("y").input(raw_path).output(
-      output_path,
-      vf="crop=in_h:in_h,scale=500:500",
-      an=None,
-    ).execute()
-
-    return output_path
+def new_deck(name):
+  return genanki.Deck(name_to_id(name), name)
 
 if __name__ == '__main__':
-  import fileinput
-  for url in fileinput.input(encoding="utf-8"):
-    video = Video(url)
-    print(video.processed_video())
+  deck = new_deck('Lifeprint ASL (custom)::Lesson 1 phrases')
+  tags = ['lesson_01', 'phrases']
+
+  media = []
+  for line in fileinput.input(encoding="utf-8"):
+    if line.strip() == "" or line.strip().startswith("#"):
+      continue
+
+    video = Video(line, cache_path=CACHE)
+    video_path = video.processed_video()
+    media.append(video_path)
+
+    basename_html = html.escape(os.path.basename(video_path))
+    deck.add_note(genanki.Note(
+      model=genanki.BASIC_AND_REVERSED_CARD_MODEL,
+      fields=[
+        html.escape(video.title()),
+        f"[sound:{basename_html}]",
+      ],
+      guid=genanki.guid_for(video.id),
+      tags=tags,
+    ))
+
+  package = genanki.Package(deck)
+  package.media_files = media
+  package.write_to_file('output.apkg')
---
+++ downloader/video.py
@@ -0,0 +1,100 @@
+from ffmpeg import FFmpeg
+import yt_dlp
+import json
+import os
+import urllib
+
+YT_DLP_OPTIONS = {
+  'quiet': True,
+  'skip_unavailable_fragments': False,
+}
+
+def yt_url_to_id(url):
+  url_info = urllib.parse.urlparse(url)
+  query = urllib.parse.parse_qs(url_info.query)
+  if len(query['v']) != 1:
+    raise ValueError(f"Expected exactly one v parameter in URL: {url}")
+  return query['v'][0]
+
+class Logger:
+  def __init__(self):
+    self.level = 0
+
+  def info(self, message):
+    if self.level >= 0:
+      print(message)
+
+LOGGER = Logger()
+
+class Video:
+  def __init__(self, url, cache_path="."):
+    self.url = url
+    self.cache_path = cache_path
+    self.id = yt_url_to_id(url)
+    self._info = None
+
+  def cached(self, filename):
+    return os.path.join(self.cache_path, filename)
+
+  def info_cache_path(self):
+    return self.cached(self.id + '_info.json')
+
+  def raw_video_cache_path(self, logger=LOGGER):
+    return self.cached(self.id + '_raw.' + self.info(logger=logger)['ext'])
+
+  def processed_video_cache_path(self):
+    return self.cached(self.id + '_processed.mp4')
+
+  def info(self, logger=LOGGER):
+    if self._info is None:
+      try:
+        with open(self.info_cache_path(), 'r', encoding="utf-8") as file:
+          self._info = json.load(file)
+      except FileNotFoundError:
+        with yt_dlp.YoutubeDL(YT_DLP_OPTIONS.copy()) as ydl:
+          logger.info(f"{self.id}: getting info")
+          self._info = ydl.extract_info(self.url, download=False)
+          with open(self.info_cache_path(), 'w', encoding="utf-8") as file:
+            file.write(json.dumps(ydl.sanitize_info(self._info)))
+    return self._info
+
+  def title(self, logger=LOGGER):
+    return self.info(logger=logger)['title']
+
+  def raw_video(self, logger=LOGGER):
+    path = self.raw_video_cache_path(logger=logger)
+    if os.path.exists(path) and os.stat(path).st_size > 0:
+      return path
+
+    logger.info(f"{self.id}: downloading raw video to {path}")
+    options = {
+      'outtmpl': {
+        'default': path,
+      },
+      **YT_DLP_OPTIONS,
+    }
+
+    with yt_dlp.YoutubeDL(options) as ydl:
+      # FIXME why not use the in-memory info?
+      error_code = ydl.download_with_info_file(self.info_cache_path())
+      if error_code:
+        # FIXME??!
+        raise RuntimeError(error_code)
+
+    return path
+
+  def processed_video(self, logger=LOGGER):
+    output_path = self.processed_video_cache_path()
+    if os.path.exists(output_path) and os.stat(output_path).st_size > 0:
+      return output_path
+
+    raw_path = self.raw_video(logger=logger)
+
+    logger.info(f"{self.id}: processing video")
+    FFmpeg().option("y").input(raw_path).output(
+      output_path,
+      vf="crop=in_h:in_h,scale=500:500",
+      an=None,
+    ).execute()
+
+    return output_path
