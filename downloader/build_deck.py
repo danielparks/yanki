c0326937d5441c99fdc06049098727b7d253d6ed
Daniel Parks <dp+git@oxidized.org>
2024-11-25 01:17:48 -0800

    Support --help and --verbose parameters.

--- downloader/build_deck.py
+++ downloader/build_deck.py
@@ -1,6 +1,18 @@
+import argparse
 import fileinput
 
 from downloader.anki import DeckParser
 
 def cli():
-  DeckParser(debug=False).parse_input(fileinput.input(encoding="utf-8"))
+  parser = argparse.ArgumentParser(
+    prog='build-deck',
+    description='Builds and Anki deck from a text file.',
+  )
+  parser.add_argument('-v', '--verbose', action='store_true')
+  parser.add_argument('path', nargs='*')
+  args = parser.parse_args()
+
+  input = fileinput.input(files=args.path, encoding="utf-8")
+  DeckParser(debug=args.verbose).parse_input(input)
+
+  return 0
