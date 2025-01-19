b355720fe0103362a0d2ef5cdfa86194e0abe262
Daniel Parks <dp+git@oxidized.org>
2025-01-18 22:38:41 -0800

    Fix: `__all__` is a list of strings, not types.

--- yanki/parser/__init__.py
+++ yanki/parser/__init__.py
@@ -7,10 +7,10 @@ from .model import DeckSpec, NoteSpec
 from .parser import DeckFilesParser
 
 __all__ = [
-    NoteConfigFrozen,
-    find_invalid_format,
-    NOTE_VARIABLES,
-    DeckSpec,
-    NoteSpec,
-    DeckFilesParser,
+    "NoteConfigFrozen",
+    "find_invalid_format",
+    "NOTE_VARIABLES",
+    "DeckSpec",
+    "NoteSpec",
+    "DeckFilesParser",
 ]
