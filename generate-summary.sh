8cff66d5bea750f24d03157ed224a2c1c04c4aae
Daniel Parks <dp+git@oxidized.org>
2025-02-01 10:59:28 -0800

    Track summary of all notes in ASL decks
    This configures pre-commit to run `generate-summary.sh` on most commits.
    It uses `yanki list-notes` to summarize all the notes in `asl/*.deck` to
    make it easy to see when they change.
    
    This will skip commits that only affect irrelevant files, such as tests,
    GitHub workflows, the README, etc.

---
+++ generate-summary.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+yanki list-notes -f '{note_id} TEXT: {text!r}
+{note_id} TAGS: {tags!r}
+{note_id} MORE: {more!r}' asl/*.deck \
+| sort \
+>asl/summary.txt
+
+git --no-pager diff --word-diff-regex=. --color=always asl/summary.txt
