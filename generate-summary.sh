13646c4ed045f93d44b66371df9f3eece52b4579
Daniel Parks <dp+git@oxidized.org>
2025-04-02 15:16:41 -0700

    ASL summary: restore previous sorting by explicitly setting collation.

--- generate-summary.sh
+++ generate-summary.sh
@@ -9,7 +9,7 @@ yanki list-notes -f '{note_id} MEDIA: {media_paths!r}
 {note_id} TAGS: {tags!r}
 {note_id} TEXT: {text!r}' asl/*.deck \
 | sed -e "/ MEDIA: /s#${cache_prefix}##g" \
-| sort \
+| LC_COLLATE=C sort \
 >asl/summary.txt
 
 git --no-pager diff --word-diff-regex=. -U1 --color=always asl/summary.txt
