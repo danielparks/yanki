0f9b1400d2944a58c0a89a07f9be7280efc8024c
Daniel Parks <dp+git@oxidized.org>
2025-06-14 10:00:16 -0700

    `Video._crop` should never be `"none"`.

--- yanki/video.py
+++ yanki/video.py
@@ -536,8 +536,6 @@ class Video:
     async def actual_crop_async(self):
         if self._crop == "auto":
             return (await self.more_info_async())["cropdetect"]
-        if self._crop == "none":
-            return None
         return self._crop
 
     async def parameters_async(self):
