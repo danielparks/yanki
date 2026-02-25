f9b08e91bebe2d3325028b423851aba2270f653b
Daniel Parks <dp+git@oxidized.org>
2026-02-24 17:35:08 -0800

    Lint RUF069: fix floating point comparison.

--- tests/video_parameters_test.py
+++ tests/video_parameters_test.py
@@ -1,4 +1,5 @@
 import logging
+from math import isclose
 
 import pytest
 
@@ -23,45 +24,44 @@ def test_time_parse():  # noqa: PLR0915 (too many statements)
     assert video.time_to_seconds("", on_none="a") == "a"
     assert video.time_to_seconds(None, on_none="a") == "a"
 
-    assert round(video.time_to_seconds("123.4"), 9) == 123.4
-    assert round(video.time_to_seconds("123.4s"), 9) == 123.4
-    assert round(video.time_to_seconds("123.4S"), 9) == 123.4
-    assert round(video.time_to_seconds("123.4ms"), 9) == 0.1234
-    assert round(video.time_to_seconds("123.4MS"), 9) == 0.1234
-    assert round(video.time_to_seconds("123.4Ms"), 9) == 0.1234
-    assert round(video.time_to_seconds("123.4mS"), 9) == 0.1234
-    assert round(video.time_to_seconds("123.4us"), 9) == 0.0001234
-    assert round(video.time_to_seconds("123.4US"), 9) == 0.0001234
-    assert round(video.time_to_seconds("123.4Us"), 9) == 0.0001234
-    assert round(video.time_to_seconds("123.4uS"), 9) == 0.0001234
-    assert round(video.time_to_seconds("-2S"), 9) == -2
+    assert isclose(video.time_to_seconds("123.4"), 123.4)
+    assert isclose(video.time_to_seconds("123.4s"), 123.4)
+    assert isclose(video.time_to_seconds("123.4S"), 123.4)
+    assert isclose(video.time_to_seconds("123.4ms"), 0.1234)
+    assert isclose(video.time_to_seconds("123.4MS"), 0.1234)
+    assert isclose(video.time_to_seconds("123.4Ms"), 0.1234)
+    assert isclose(video.time_to_seconds("123.4mS"), 0.1234)
+    assert isclose(video.time_to_seconds("123.4us"), 0.0001234)
+    assert isclose(video.time_to_seconds("123.4US"), 0.0001234)
+    assert isclose(video.time_to_seconds("123.4Us"), 0.0001234)
+    assert isclose(video.time_to_seconds("123.4uS"), 0.0001234)
+    assert isclose(video.time_to_seconds("-2S"), -2)
 
-    assert round(video.time_to_seconds("2 s"), 9) == 2
+    assert isclose(video.time_to_seconds("2 s"), 2)
 
     with pytest.raises(ValueError) as error_info:
         video.time_to_seconds("2ks")
     LOGGER.info("Caught exception", exc_info=error_info.value)
     assert error_info.match("could not convert string to float")
 
-    assert round(video.time_to_seconds("0:45.6"), 9) == 45.6
-    assert round(video.time_to_seconds("1:45.6"), 9) == 60 + 45.6
-    assert round(video.time_to_seconds("1:1:45.6"), 9) == 3600 + 60 + 45.6
-    assert (
-        round(video.time_to_seconds("1:23:45.6"), 9)
-        == (1 * 60 + 23) * 60 + 45.6
+    assert isclose(video.time_to_seconds("0:45.6"), 45.6)
+    assert isclose(video.time_to_seconds("1:45.6"), 60 + 45.6)
+    assert isclose(video.time_to_seconds("1:1:45.6"), 3600 + 60 + 45.6)
+    assert isclose(
+        video.time_to_seconds("1:23:45.6"), (1 * 60 + 23) * 60 + 45.6
     )
-    assert (
-        round(video.time_to_seconds("02:03:05.6"), 9) == (2 * 60 + 3) * 60 + 5.6
-    )
+    assert isclose(video.time_to_seconds("02:03:05.6"), (2 * 60 + 3) * 60 + 5.6)
 
-    assert round(video.time_to_seconds("-0:45.6"), 9) == -45.6
-    assert round(video.time_to_seconds("-1:45.6"), 9) == -(60 + 45.6)
-    assert round(video.time_to_seconds("-1:1:45.6"), 9) == -(3600 + 60 + 45.6)
-    assert round(video.time_to_seconds("-1:23:45.6"), 9) == -(
-        (1 * 60 + 23) * 60 + 45.6
+    assert isclose(video.time_to_seconds("-0:45.6"), -45.6)
+    assert isclose(video.time_to_seconds("-1:45.6"), -(60 + 45.6))
+    assert isclose(video.time_to_seconds("-1:1:45.6"), -(3600 + 60 + 45.6))
+    assert isclose(
+        video.time_to_seconds("-1:23:45.6"),
+        -((1 * 60 + 23) * 60 + 45.6),
     )
-    assert round(video.time_to_seconds("-02:03:05.6"), 9) == -(
-        (2 * 60 + 3) * 60 + 5.6
+    assert isclose(
+        video.time_to_seconds("-02:03:05.6"),
+        -((2 * 60 + 3) * 60 + 5.6),
     )
 
     with pytest.raises(ValueError) as error_info:
@@ -80,16 +80,15 @@ def test_time_parse():  # noqa: PLR0915 (too many statements)
     assert error_info.match("could not convert string to float")
 
     # FIXME? Should probably be error
-    assert (
-        round(video.time_to_seconds("2.1:3:5.6"), 9)
-        == (2.1 * 60 + 3) * 60 + 5.6
+    assert isclose(
+        video.time_to_seconds("2.1:3:5.6"), (2.1 * 60 + 3) * 60 + 5.6
     )
 
     # Frames
-    assert round(video.time_to_seconds("0f"), 9) == 0
-    assert round(video.time_to_seconds("1F"), 9) == round(1 / 60, 9)
-    assert round(video.time_to_seconds("2F"), 9) == round(2 / 60, 9)
-    assert round(video.time_to_seconds("-2F"), 9) == round(-2 / 60, 9)
+    assert isclose(video.time_to_seconds("0f"), 0)
+    assert isclose(video.time_to_seconds("1F"), 1 / 60)
+    assert isclose(video.time_to_seconds("2F"), 2 / 60)
+    assert isclose(video.time_to_seconds("-2F"), -2 / 60)
 
     with pytest.raises(ValueError) as error_info:
         video.time_to_seconds("2fF")
